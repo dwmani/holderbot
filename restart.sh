b6da831b1b9d42a3c73e572f9a95e356e2cc344d
dwmani <162987327+dwmani@users.noreply.github.com>
2024-05-28 13:46:59 +0330

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,6 +4,7 @@ pkill -f monitoring.py
 pkill -f holder.py 
 pkill -f expired.py 
 pkill -f limiteder.py 
+pkill -f actived.py
 cd
 cd /holderbot
 source hold/bin/activate
@@ -11,7 +12,9 @@ chmod +x monitoring.py
 chmod +x holder.py
 chmod +x expired.py
 chmod +x limiteder.py
+chmod +x actived.py
 nohup python3 monitoring.py & disown
 nohup python3 holder.py & disown
 nohup python3 expired.py & disown
 nohup python3 limiteder.py & disown
+nohup python3 actived.py & disown
